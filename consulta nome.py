13adaf53a86e7e317720af70937d8a5ab1aeee32
Hacker <133931634+Hacker-github-ctrl@users.noreply.github.com>
2023-11-30 21:06:18 -0300

    Create consulta nome.py

---
+++ consulta nome.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import time
+
+nome = input("Digite o seu nome: ")
+if nome == "":
+  print("\nOlá", nome)
+  print("\nSeja bem Vindo ao mundo da Tecnologia")
+  time.sleep(3)
+  exit()
+
+
